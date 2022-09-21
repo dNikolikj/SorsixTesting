8ba87661937ba2108e84e2e7d842789da97449fa
dNikolikj <darenikolic@hotmail.com>
2022-09-21 02:05:09 +0200

    helper

---
+++ SorsixTasks/SorsixTask_1/FirstTaskHelpers/FistTaskHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace SorsixTesting.Helper.FirstTaskHelpers
+{
+    public static class FistTaskHelper
+    {
+        public static string IsStringCorrect(string inputLine)
+        {
+            string trimmedInput = inputLine.Trim();
+
+            Dictionary<char, int> dictionary = ListOfKeysFromDictionary(trimmedInput);
+
+            for (int i = 0; i < trimmedInput.Length; i++)
+            {
+                if (dictionary.ContainsKey(trimmedInput[i]))
+                {
+                    dictionary[trimmedInput[i]]++;
+                }
+            }
+
+            if (dictionary.Values.Distinct().Count() == 1)
+            {
+                Console.WriteLine("CorrectString");
+                return "Correct string!";
+            }
+            else if (dictionary.Values.Distinct().Count() == 2)
+            {
+                int checkHelper = dictionary.Values.Distinct().OrderBy(x => x).First();
+                if (checkHelper == 1 && dictionary.Select(x => x.Value == 1).ToList().Count == 1)
+                {
+                    Console.WriteLine("CorrectString");
+                    return "Correct string!";
+                }
+                else if (dictionary.Values.Distinct().OrderByDescending(x => x).First() == (dictionary.Values.Distinct().OrderByDescending(x => x).Last() + 1))
+                {
+                    Console.WriteLine("CorrectString");
+                    return "Correct string!";
+                }
+                else
+                {
+                    
+                    return "Wrong string!";
+                }
+            }
+
+            Console.WriteLine("Wrong string!");
+            return "Wrong string!";
+
+        }
+        public static Dictionary<char, int> ListOfKeysFromDictionary(string input)
+        {
+            string distincInput = string.Join("", input.ToCharArray().Distinct());
+
+            Dictionary<char, int> keysResult = new Dictionary<char, int>();
+            foreach (var item in distincInput)
+            {
+                keysResult[item] = 0;
+            }
+
+            return keysResult;
+        }
+    }
+}
---
+++ SorsixTasks/SorsixTask_2/SecondTaskHelpers/SecondTaskHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SorsixTask_2.SecondTaskHelpers
+{
+    public static  class SecondTaskHelper
+    {
+        public static string FindTheMinimumAndTheMaximumNumber(int input)
+        {
+            if (input.ToString().StartsWith("0"))
+            {
+                return "The number cannot be 0 or negative!";
+            }
+
+            int maxNumber = 0;
+            int minNumber = 0;
+
+            string numbers = input.ToString();
+            int maxNumberDigit = 0;
+            int minNumberDigit = 10;
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (int.Parse(numbers[i].ToString()) > maxNumberDigit)
+                {
+                    maxNumberDigit = int.Parse(numbers[i].ToString());
+                }
+                if (int.Parse(numbers[i].ToString()) < minNumberDigit)
+                {
+                    minNumberDigit = int.Parse(numbers[i].ToString());
+                }
+            }
+
+            if (!(int.Parse(numbers.ToCharArray()[0].ToString()) == maxNumberDigit))
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (int.Parse(numbers[i].ToString()) == maxNumberDigit)
+                    {
+                        maxNumber = int.Parse(Switch(numbers, i, 0));
+                    }
+                }
+            }
+            else maxNumber = input;
+
+
+            if (!(int.Parse(numbers.ToCharArray()[0].ToString()) == minNumberDigit))
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (int.Parse(numbers[i].ToString()) == minNumberDigit)
+                    {
+                        minNumber = int.Parse(Switch(numbers, i, 0));
+                    }
+                }
+            }
+            else minNumber = input;
+
+            Console.WriteLine(maxNumber);
+            return $"The minimum number is :{minNumber}, while the maximum number of the given input is {maxNumber}.";
+        }
+
+        public static string Switch(string input, int x, int y)
+        {
+            char[] chars = input.ToCharArray();
+
+            char tempChar = input[x];
+
+            chars[x] = input[y];
+            chars[y] = tempChar;
+
+            return String.Join("", chars);
+        }
+    }
+}
